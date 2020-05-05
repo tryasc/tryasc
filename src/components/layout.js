676eb89f849df5f1a8d50bb3164a7a2e3c03bb22
Pete Broderick <pete@simplybinary.com>
2020-05-05 16:06:40 -0400

    Fix height a bit

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,7 +34,7 @@ const Layout = ({ children }) => {
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
-          marginTop: 400,
+          marginTop: 200,
         }}
     >
         <Announcements />
