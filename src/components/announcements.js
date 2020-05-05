3bc7c02879843e71066a714f6bc816280bd32c0e
lkbroderick <lkbroderick@gmail.com>
2020-05-05 16:23:40 -0400

    Update announcements.js

--- src/components/announcements.js
+++ src/components/announcements.js
@@ -21,8 +21,8 @@ const Announcements = () => {
   return (
     <div className="announcements">
       <p>Due to COVID-19 and the uncertainty of times ahead, we are working on a new registration process for the 2020 season. We have set up an online Google Form for preliminary registration. No money will be collected at this time. Please be aware this does not guarantee there will be a 2020 season, and your registration is not complete until payment is received.  If the 2020 season is approved, birth certificates, additional forms, and payment will be collected.</p>
-      <p>In order to cover our expenses, but still help families who have been affected financially during this time, we have opted to use a lower quality uniform this year so we can lower the registration price.</p>
-      <p>Registration Cost for 2020 - Tackle Football and Cheerleading: $125 / Flag Football: $50</p>
+      <p>In order to help families who have been affected financially during this time, we have opted to use a lower quality uniform to lower the registration costs for 2020.</p>
+      <p>Tackle Football and Cheerleading: $125 / Flag Football: $50</p>
       <p>*Please Note: Registration is still on a FIRST COME, FIRST SERVED basis.  Sign up early to reserve your spot!</p>
     </div>
   )
