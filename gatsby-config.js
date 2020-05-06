f76140cb60d75d487eb0120282d11c2a37e9c0ea
Pete Broderick <pete@simplybinary.com>
2020-05-06 17:28:44 -0400

    Update description / author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `TRYA`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Travelers Rest Youth Association`,
+    author: `@tryasc`,
   },
   plugins: [
     {
