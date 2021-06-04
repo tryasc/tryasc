bbddd129afb2c329ac0ff0d6170fe0720acbbede
Pete Broderick <pete@simplybinary.com>
2021-06-04 12:27:05 -0400

    Fix markup

--- src/components/announcements.js
+++ src/components/announcements.js
@@ -1,38 +1,49 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
-import {
-  AppBar,
-  Toolbar,
-  Typography
-} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles"
+import { AppBar, Toolbar, Typography } from "@material-ui/core"
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    '& > * + *': {
+    "& > * + *": {
       marginLeft: theme.spacing(2),
     },
   },
-}));
+}))
 
 const Announcements = () => {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <div className="announcements">
-      <p><b>**TACKLE FOOTBALL**</b>
-        Uniform fittings will be held at First Team (2520 Wade Hampton Blvd, Unit A, Greenville, SC  29615). Please bring your own shoulder pads.  If you need to purchase shoulder pads, First Team will be selling them at a discounted rate to TRYA players.
-         <li> 4:00-6:00pm
-         <li> June 7 - 8u
-         <li> June 9 - 10u
-         <li> June 14 - 12u 
-         <li> June 16 - make-up
-   <p style={{ borderTop: "1px dashed #333333", paddingTop: "25px" }}></p>
-   <p><b>**10u FOOTBALL PLAYERS**</b>
-     We are excited to annouce we have enough registrants for two 10u teams!  We will be holding player evaluations in order to assign teams in the most fair way possible.  
-      <li>Monday, June 14th
-      <li>6:00pm
-      <li>Trailblazer Park, upper field
+      <p>
+        <b>**TACKLE FOOTBALL**</b>
+        <br />
+        Uniform fittings will be held at First Team (2520 Wade Hampton Blvd,
+        Unit A, Greenville, SC 29615). Please bring your own shoulder pads. If
+        you need to purchase shoulder pads, First Team will be selling them at a
+        discounted rate to TRYA players.
+        <ul>
+          <li>4:00-6:00pm</li>
+          <li>June 7 - 8u</li>
+          <li>June 9 - 10u</li>
+          <li>June 14 - 12u</li>
+          <li>June 16 - make-up</li>
+        </ul>
+      </p>
+      <p style={{ borderTop: "1px dashed #333333", paddingTop: "25px" }}></p>
+      <p>
+        <b>**10u FOOTBALL PLAYERS**</b>
+        <br />
+        We are excited to annouce we have enough registrants for two 10u teams!
+        We will be holding player evaluations in order to assign teams in the
+        most fair way possible.
+        <ul>
+          <li>Monday, June 14th</li>
+          <li>6:00pm</li>
+          <li>Trailblazer Park, upper field</li>
+        </ul>
+      </p>
     </div>
   )
 }
--- src/pages/football.js
+++ src/pages/football.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Announcements from "../components/announcements"
 
 const FootballPage = () => (
   <Layout>
     <SEO title="Football" />
+    <Announcements />
     <h1>TRYA DEVILDOG FOOTALL</h1>
 
     <p>
