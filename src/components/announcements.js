43e6d16e1e9a873280ec0faf18b0f2cc09dcd4d2
lkbroderick <lindsay@simplybinary.com>
2021-06-04 11:21:18 -0400

    Update announcements.js

--- src/components/announcements.js
+++ src/components/announcements.js
@@ -20,15 +20,20 @@ const Announcements = () => {
   const classes = useStyles();
   return (
     <div className="announcements">
-      <p>Greetings, TRYA Families. We are incredibly excited to be back on the fields for games and practice! As school and other activities begin, staying healthy and avoiding the spread of sickness (even in areas not related to Covid) has always been important to our organization. We want to be careful and need your help in keeping everyone well! Please use the following guidelines:</p>
-      <ul>
-        <li>If your athlete or anyone in direct contact with your athlete is sick, please stay home.</li>
-        <li>Communication is key. Please communicate with your coaches about any sickness.</li>
-        <li>Staying home due to illness will not impact an athlete’s playing time or position on the team/squad.</li>
-        <li>We need everyone’s help and cooperation keeping our athletes safe and healthy on and off the field.</li>
-      </ul>
-
-      <p>We look forward to a great season ahead, and Go Devildogs!</p>
+      <p><b>**TACKLE FOOTBALL**</b>
+        Uniform fittings will be held at First Team (2520 Wade Hampton Blvd, Unit A, Greenville, SC  29615). Please bring your own shoulder pads.  If you need to purchase shoulder pads, First Team will be selling them at a discounted rate to TRYA players.
+         <li> 4:00-6:00pm
+         <li> June 7 - 8u
+         <li> June 9 - 10u
+         <li> June 14 - 12u 
+         <li> June 16 - make-up
+  ______________________________________________________________________________________________________________________________
+  
+   <p><b>**10u FOOTBALL PLAYERS**</b>
+     We are excited to annouce we have enough registrants for two 10u teams!  We will be holding player evaluations in order to assign teams in the most fair way possible.  
+      <li>Monday, June 14th
+      <li>6:00pm
+      <li>Trailblazer Park, upper field
     </div>
   )
 }
