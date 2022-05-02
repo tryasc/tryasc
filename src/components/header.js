e568b23bfa9bbcadf149269faebb37d06fd74efa
Pete Broderick <pete@simplybinary.com>
2022-05-02 10:59:58 -0400

    Add registration and fix menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,21 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import { TrLogo } from "./tr"
-import { AppBar, Toolbar, Typography } from "@material-ui/core"
+import { Menu, Container, MenuItem, Box, IconButton, AppBar, Toolbar, Typography } from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu";
 
 const Header = ({ siteTitle }) => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -13,13 +24,12 @@ const Header = ({ siteTitle }) => {
         backgroundColor: "#0834b7",
       }}
     >
-      <div
-        className="top-nav"
+    <Container maxWidth="xl" className="top-nav"
         style={{
           margin: `0 auto`,
           width: 940,
         }}
-      >
+    >
         <Toolbar
           style={{
             padding: 0,
@@ -31,35 +41,95 @@ const Header = ({ siteTitle }) => {
               <TrLogo /> {siteTitle}
             </Link>
           </Typography>
-          <ul style={{ marginBottom: 0, paddingRight: "10px" }}>
-            <li>
-              <Link to="/" activeClassName="active">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/football/" activeClassName="active">
-                Football
-              </Link>
-            </li>
-            <li className="cheerleading">
-              <Link to="/cheerleading/" activeClassName="active">
-                Cheerleading
-              </Link>
-            </li>
-            <li className="restival">
-              <Link to="/restival/" activeClassName="active">
-                Travelers Restival
-              </Link>
-            </li>
-            <li className="auction">
-              <Link to="/auction/" activeClassName="active">
-                Silent Auction
-              </Link>
-            </li>
-          </ul>
+          <Box style={{ marginTop: "-75px", padding: 0}} display={{ xs: 'none', md: 'flex' }}>
+            <ul style={{ marginBottom: 0, paddingRight: "10px" }}>
+              <li>
+                <Link to="/" activeClassName="active">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/football/" activeClassName="active">
+                  Football
+                </Link>
+              </li>
+              <li className="cheerleading">
+                <Link to="/cheerleading/" activeClassName="active">
+                  Cheerleading
+                </Link>
+              </li>
+              <li className="restival">
+                <Link to="/restival/" activeClassName="active">
+                  Travelers Restival
+                </Link>
+              </li>
+              <li className="auction">
+                <Link to="/auction/" activeClassName="active">
+                  Silent Auction
+                </Link>
+              </li>
+            </ul>
+          </Box>
+          <Box display={{ xs: 'flex', md: 'none' }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              display={{ xs: 'block', md: 'none' }}
+              style={{ float: 'right' }}
+            >
+              <MenuItem key='football' onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link to="/football/" activeClassName="active">
+                    Football
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem key='cheerleading' onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link to="/cheerleading/" activeClassName="active">
+                    Cheerleading
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem key='restival' onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link to="/restival/" activeClassName="active">
+                    Travelers Restival
+                  </Link>
+                </Typography>
+              </MenuItem>
+              <MenuItem key='auction' onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">
+                  <Link to="/auction/" activeClassName="active">
+                    Silent Auction
+                  </Link>
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>
-      </div>
+      </Container>
     </AppBar>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,33 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div class="updates">
       <h2>2022 Updates</h2>
-      <p>Fall registration for Football and Cheer is not yet open.</p>
+      <h4>ONLINE REGISTRATION FOR FOOTBALL AND CHEER IS NOW OPEN THROUGH MAY 15</h4>
+      <p>
+        <b>***ONLY for those submitting ALL paperwork and payment online!!***</b>
+        <br/>
+      </p>
+      <p>
+        Paperwork must be uploaded via the registration form at the time of registration.<br/>
+        -Birth Certificate (football only)<br/>
+        -Greenville Rec Football Waiver (football & cheer)<br/>
+        -Greenville Rec player & parent code of conduct (football & cheer)<br/>
+        Copies of all required Greenville Rec forms will be in the registration form or can be found on our website.<br/>
+        *online football registrants will still need to come in-person for pictures and weight.<br/>
+        Once online registration is submitted, payment requests will be sent within 1 business day.  Online payment methods are zelle or Venmo.
+      </p>
+      <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfoyAco8EwepBoQAuOAPhs3pBHGurFS6cwLBdnqKg_gpeO_Ow/viewform">FOOTBALL REGISTRATION FORM</a></p>
+      <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSeZw7PTAm9qIEZtyr1xKi7q8KWuKTV4jadGCJ8Xk66q5S5yvA/viewform">CHEER REGISTRATION FORM</a></p>
+      <hr/>
+
+      <h4>IN-PERSON REGISTRATION IS at TR CITY HALL @ Trailblazer Park, May 18th (6pm-8pm) & May 21st (10am - 1pm)</h4>
+      <p>Football must bring a birth certificate or copy of birth certificate.  Payment accepted will be cash, money order, cashier's check, personal check, or credit card with an additional processing fee.<br/>
+        **cash needs to be in a sealed/marked envelope. This should be completed prior to arrival. No change accepted. Returned checks subject to a $39 processing fee.</p>
+
+      <p>
+        Registration fees are as follows:<br/>
+      Football = $180<br/>
+      Cheer = $160<br/>
+      </p>
     </div>
     <h2>Mission</h2>
     <p>
