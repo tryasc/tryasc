867f07f02964253669b738f5a5196237fde9fc12
Pete Broderick <pete@simplybinary.com>
2023-05-05 10:34:45 -0400

    update restival dates

--- src/pages/restival.js
+++ src/pages/restival.js
@@ -12,18 +12,16 @@ const RestivalPage = () => (
       <img src={RestivalImg} alt="Travelers Restival" width="500" />
     </p>
     <p>
-      We are excited to announce our 3rd annual community-wide Travelers
+      We are excited to announce our 4th annual community-wide Travelers
       Restival!
       <br />
       <br />
-      Saturday, August 20, 2022
+      Saturday, August 19, 2023
       <br />
       3 PM - 8 PM
       <br />
       Trailblazer Park
     </p>
-    <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSdRkYnsokPz4uVTsY76E5qT0Kk8CTENJfskD7xLDnewTOciGA/viewform">2022 Travelers Restival Sponsor Form</a></p>
-    <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSe0i80iG4rC5_PWvvYkXRSgSwYYQBlzGKrkaT9M6rHKd-dkZQ/viewform">2022 Travelers Restival Market Vendor & Food Vendor Application</a></p>
      <p>
       Please direct questions to{" "}
       <a href="mailto:travelersrestival@gmail.com">
